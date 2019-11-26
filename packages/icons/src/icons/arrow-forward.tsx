a43f205718bc349609bcfb6398092d0accbe48d6
Aleksandra Shalamova <asshalamova@avito.ru>
2019-11-26 12:22:20 +0300

    DP-1463 add more icons and fix linter

--- packages/icons/src/icons/arrow-forward.tsx
+++ packages/icons/src/icons/arrow-forward.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { createIcon } from '../createIcon'
 
 createIcon({
-    name: 'arrow-forward',
-    originalWidth: 24,
-    originalHeight: 24,
-    svg: (
-        <path
-            d='M16.586 13l-5.293 5.293a1 1 0 001.414 1.414l7-7a1 1 0 000-1.414l-7-7a1 1 0 10-1.414 1.414L16.586 11H5a1 1 0 100 2h11.586z'
-        />
-    ),
+  name: 'arrow-forward',
+  originalWidth: 24,
+  originalHeight: 24,
+  svg: (
+    <path
+      d='M16.586 13l-5.293 5.293a1 1 0 001.414 1.414l7-7a1 1 0 000-1.414l-7-7a1 1 0 10-1.414 1.414L16.586 11H5a1 1 0 100 2h11.586z'
+    />
+  ),
 })
--- packages/icons/src/icons/menu.tsx
+++ packages/icons/src/icons/menu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import { createIcon } from '../createIcon'
 
 createIcon({
@@ -8,17 +8,17 @@ createIcon({
   svg: (
     <g>
       <mask
-          id='Menu_svg__a'
-          maskUnits='userSpaceOnUse'
-          x={3}
-          y={5}
-          width={18}
-          height={14}
+        id='Menu_svg__a'
+        maskUnits='userSpaceOnUse'
+        x={3}
+        y={5}
+        width={18}
+        height={14}
       >
         <path
-            fillRule='evenodd'
-            clipRule='evenodd'
-            d='M21 6a1 1 0 00-1-1H4a1 1 0 000 2h16a1 1 0 001-1zm0 12a1 1 0 00-1-1h-8a1 1 0 100 2h8a1 1 0 001-1zm-1-7a1 1 0 110 2H8a1 1 0 110-2h12z'
+          fillRule='evenodd'
+          clipRule='evenodd'
+          d='M21 6a1 1 0 00-1-1H4a1 1 0 000 2h16a1 1 0 001-1zm0 12a1 1 0 00-1-1h-8a1 1 0 100 2h8a1 1 0 001-1zm-1-7a1 1 0 110 2H8a1 1 0 110-2h12z'
         />
       </mask>
       <g mask='url(#Menu_svg__a)'>
