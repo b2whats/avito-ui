a3343a3ce96cdffcaead3182ea5653d7e49dc975
vyuklepov <vyuklepov@avito.ru>
2020-04-06 16:34:43 +0300

    MDP-985: Allow component overrides in ThemeProvider

--- packages/core/src/theme/ThemeProvider.tsx
+++ packages/core/src/theme/ThemeProvider.tsx
@@ -3,8 +3,9 @@ import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming'
 import { tokens, Tokens } from '@avito/tokens'
 import { mergeTheme } from './mergeTheme'
 import { useTheme } from './useTheme'
+import { Theme } from './contract'
 
-type ProviderProps = { children: React.ReactNode, defaultTheme?: Tokens, theme?: Partial<Tokens> }
+type ProviderProps = { children: React.ReactNode, defaultTheme?: Tokens, theme?: Partial<Theme> }
 
 export function ThemeProvider({ defaultTheme = tokens, theme, children }: ProviderProps) {
   const globalTheme = useTheme()
