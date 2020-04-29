a20b81ff4b25360237e5e2b7c26b682d3eb9cfce
Vladimir Klepov <vyuklepov@avito.ru>
2020-04-29 19:06:30 +0300

    MDP-1140: Add pik text styles

--- packages/web-components/src/themes/pik.ts
+++ packages/web-components/src/themes/pik.ts
@@ -1,3 +1,29 @@
-import { Theme, DeepPartial } from '@avito/core'
+import { Theme, dsl, DeepPartial } from '@avito/core'
 
-export const pikTheme: DeepPartial<Theme> = {}
+export const pikTheme: DeepPartial<Theme> = {
+  font: {
+    fontFamily: 'Avito,"Helvetica Neue",Helvetica,sans-serif',
+    fontSize: {
+      xl: 24,
+    },
+    smoothing: {
+      webkit: 'antialiased',
+      moz: 'grayscale',
+    },
+  },
+  Text: {
+    scheme: {
+      Text: {
+        size: dsl.styleMap('lineHeight', {
+          xs: 1.23,
+          s: 1.43,
+          m: 1.5,
+          l: 1.4,
+          xl: 1.34,
+          xxl: 1.25,
+          xxxl: 1.2,
+        }),
+      },
+    },
+  },
+}
