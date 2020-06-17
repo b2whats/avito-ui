b25e3a54c30dc9aa70d46bd86a35e3ac425b2834
Vladimir Klepov <vyuklepov@avito.ru>
2020-06-17 13:02:10 +0300

    TERIVIAL: Fix filterMarkdown units

--- styleguidist/utils.test.ts
+++ styleguidist/utils.test.ts
@@ -1,11 +1,11 @@
-import { filterMarkdown  } from './utils'
+import { filterMarkdown } from './utils'
 
 describe('utils', () => {
   describe(filterMarkdown, () => {
-    const makeMd = (...items: string[]) => items.map(content => ({
-      type: 'markdown' as 'markdown',
-      content: content.replace(/\n\s+/g, '\n'),
-    }))
+    const makeMd = (...items: (string | any)[]) => items.map(content =>
+      typeof content === 'string'
+      ? ({ type: 'markdown', content: content.replace(/\n\s+/g, '\n') }) as any
+      : content)
 
     describe('filters by platform', () => {
       it('removes blocks for other platforms', () => {
@@ -17,15 +17,33 @@ describe('utils', () => {
           ::: platform mobile
           mobile
           :::
-        `), 'web')).toMatchObject([
-          { type: 'markdown', content: '\nyes' },
-          { type: 'markdown', content: '\nweb' },
-          { type: 'skip' },
-        ])
+        `), 'web')).toMatchObject(makeMd(`
+          yes
+          web
+        `))
+      })
+      it('removes partitioned blocks', () => {
+        expect(filterMarkdown(makeMd(`
+          yes
+          ::: platform mobile
+        `, `
+          disabled content
+        `, `
+          :::
+          end
+        `), 'web')).toMatchObject(makeMd(`
+          yes`,
+        { type: 'skip' }, `
+          end
+        `))
       })
     })
 
     describe('detects errors', () => {
+      let spy: any
+      beforeEach(() => spy = jest.spyOn(console, 'error').mockImplementation())
+      afterEach(() => spy.mockRestore())
+
       it('unexpected block end', () => {
         expect(() => filterMarkdown(makeMd(`
           :::
