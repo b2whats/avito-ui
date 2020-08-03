38acc626f66fbce7a63e09824f1a0497afc87ab4
Vladimir Klepov <vyuklepov@avito.ru>
2020-08-03 16:47:42 +0300

    chore: Update snapshots (+ custom react component type serializer)

--- packages/core/test/setupReactJest.js
+++ packages/core/test/setupReactJest.js
@@ -1,3 +1,19 @@
 import { cleanup } from '@testing-library/react/pure'
+import { isValidElementType } from 'react-is'
 
 afterEach(cleanup)
+
+// Serialize component definitions to displayName
+expect.addSnapshotSerializer({
+  test(value) {
+    return value &&
+      // most strings are not components (and when they are, they serialize well enough)
+      typeof value !== 'string' &&
+      // not all functions are components - test for CamelCase
+      (typeof value !== 'function' || /^[A-Z]/.test(value.name)) &&
+      isValidElementType(value)
+  },
+  print(value) {
+    return `#${value.displayName || value.name || 'Component'}`
+  },
+})
