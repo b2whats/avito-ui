23085be52e59018eed752519b1989577656ccf1c
Vladimir Klepov <vyuklepov@avito.ru>
2020-08-06 17:50:56 +0300

    MDP-1772: Do not apply snapshot diff to components

--- packages/core/test/theme.ts
+++ packages/core/test/theme.ts
@@ -1,4 +1,3 @@
-import { diff } from 'deep-object-diff'
 import { foldThemeParams } from '../src'
 import { flattenSets, prettyProps } from './helpers'
 
@@ -14,11 +13,34 @@ export function describeTheme(name: string, theme: any, propValues: any = {}) {
       flattenSets(propValues.sets || [])
         // we already havea base snapshot
         .filter(props => Object.keys(props).length)
-        .forEach(props => expect(diff(base, applyTheme(theme, props))).toMatchSnapshot(prettyProps(props)))
+        .forEach(props => expect(schemeDiff(base, applyTheme(theme, props))).toMatchSnapshot(prettyProps(props)))
     })
   })
 }
 
 function applyTheme(theme: any, props: any) {
   return foldThemeParams(theme.mapProps({ ...theme.defaultProps, ...props }), theme)
+}
+
+const isObject = (obj: any) => typeof obj === 'object' && obj != null
+function schemeDiff(before: object, after: object) {
+  const result = {}
+  for (const newKey in after) {
+    if (before[newKey] === after[newKey]) continue
+    // recurse on objects, but not components
+    if (isObject(before[newKey]) && isObject(after[newKey]) && newKey !== 'component') {
+      const subDiff = schemeDiff(before[newKey], after[newKey])
+      if (Object.keys(subDiff).length) {
+        result[newKey] = subDiff
+      }
+    } else {
+      result[newKey] = after[newKey]
+    }
+  }
+  for (const oldKey in before) {
+    if (!(oldKey in after)) {
+      result[oldKey] = undefined
+    }
+  }
+  return result
 }
